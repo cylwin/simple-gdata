e1b778f066dda268e9e677de7a8e4d2e3b66d8d8
Cyril BAREME <cyril.bareme@gmail.com>
2015-03-30 21:54:59 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+var request = require('request-json');
+
+
+module.exports = function (access_token) {
+
+  var client = request.createClient('');
+
+  client.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+  client.headers['GData-Version'] = '2';
+  client.headers['Authorization'] = 'Bearer' + ' ' + access_token;
+
+  client.getFeed = function (url, callback) {
+
+    if(url.lastIndexOf('alt=json') == -1) {
+      if(url.lastIndexOf('?') > -1) {
+        url += '&alt=json';
+      } else {
+        url += '?alt=json';
+      }
+    }
+    console.log(url);
+    client.get(url, function(err, res, body) {
+      return callback(err, body);
+    });
+
+  };
+
+  return client;
+};
